de20101f3d10e9bb9508db94dca43d946d6e17ed
anoopmaurya <anoop.maurya@BRILLIO.COM>
2019-08-14 19:41:19 +0530

    added tagging bat file

---
+++ git_tag.bat
@@ -0,0 +1,27 @@
+::printing current branch
+git branch
+
+::changing working branch to master
+git checkout master
+
+::pulling Updates in master
+git pull
+
+::merging dev/feature/test to master and pushing changes to remote repo
+git merge develop
+git push
+
+::preparing tag name
+SET tagname="ewb-%date%-%time%"
+
+
+::tagging master branch with tagname+date
+git tag -a %tagname% -m "Creating tag with %tagname%"
+
+
+::pushing the tag changes to remote repo
+git push origin %tagname%
+
+
+::verify tags
+git tag --list
